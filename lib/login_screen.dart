6b3a2519867ec23ea05fab92c68e7736cf2ba242
aarif29 <muhammadariftrianto2908@gmail.com>
2025-04-14 14:55:36 +0700

    update ui

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'widget/navbar_overlay.dart'; // Import BerandaScreen
+import 'package:get/get.dart';
+import '../widget/navbar_overlay.dart'; // Impor BerandaScreen
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -13,34 +14,31 @@ class _LoginScreenState extends State<LoginScreen> {
   String _user = '';
   String _password = '';
 
-
   void _attemptLogin() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
       if (_user == 'user' && _password == 'admin') {
         // Login sukses -> navigasi ke BerandaScreen
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => BerandaScreen()),
-        );
+        Get.off(() => const BerandaScreen());
       } else {
-        // Login gagal -> tampilkan alert
-        showDialog(
-          context: context,
-          builder: (context) {
-            return AlertDialog(
-              title: const Text('Login Ditolak'),
-              content: const Text('User atau password salah'),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text('OK'),
-                ),
-              ],
-            );
-          },
+        // Login gagal -> tampilkan snackbar
+        Get.snackbar(
+          'Error',
+          'User atau password salah',
+          backgroundColor: Colors.red,
+          colorText: Colors.white,
+          snackPosition: SnackPosition.BOTTOM,
+          duration: const Duration(seconds: 1),
+          titleText: const Text(
+            'Error',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          messageText: const Text(
+            'User atau password salah',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 16),
+          ),
         );
       }
     }
@@ -49,7 +47,11 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
+      appBar: AppBar(
+        title: const Text('Login'),
+        backgroundColor: Color.fromARGB(255, 40, 40, 40), // Ganti AppColors.primary dengan Colors.blue
+        iconTheme: const IconThemeData(color: Colors.white),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Form(
@@ -57,31 +59,42 @@ class _LoginScreenState extends State<LoginScreen> {
           child: Column(
             children: [
               TextFormField(
-                decoration: const InputDecoration(
+                decoration: InputDecoration(
                   labelText: 'User',
-                  border: OutlineInputBorder(),
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.blue), // Ganti AppColors.primary
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  labelStyle: const TextStyle(color: Colors.blue), // Ganti AppColors.primary
                 ),
-                validator:
-                    (value) =>
-                        value!.isEmpty ? 'User tidak boleh kosong' : null,
+                validator: (value) => value!.isEmpty ? 'User tidak boleh kosong' : null,
                 onSaved: (value) => _user = value!,
               ),
               const SizedBox(height: 20),
               TextFormField(
-                decoration: const InputDecoration(
+                decoration: InputDecoration(
                   labelText: 'Password',
-                  border: OutlineInputBorder(),
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.blue), // Ganti AppColors.primary
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  labelStyle: const TextStyle(color: Colors.blue), // Ganti AppColors.primary
                 ),
                 obscureText: true,
-                validator:
-                    (value) =>
-                        value!.isEmpty ? 'Password tidak boleh kosong' : null,
+                validator: (value) => value!.isEmpty ? 'Password tidak boleh kosong' : null,
                 onSaved: (value) => _password = value!,
                 onFieldSubmitted: (value) => _attemptLogin(),
               ),
               const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: _attemptLogin,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue, // Ganti AppColors.primary
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
                 child: const Text(
                   'Login',
                   style: TextStyle(color: Colors.white, fontSize: 17.0),
