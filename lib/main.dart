fae4842241c958b3bc228d9e6cad1e6b5abbb57e
aarif29 <muhammadariftrianto2908@gmail.com>
2025-04-17 16:12:47 +0700

    get login logout

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'splash_screen.dart';
 import 'package:get/get.dart';
+import 'login_screen.dart';
+import 'widget/navbar_overlay.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting('id_ID', null);
+  await initializeStorage(); // Inisialisasi GetStorage
   runApp(const MyApp());
 }
 
@@ -16,18 +19,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Aplikasi Sistem Manajemen Survey PLN',
-      home: SplashScreen(),
+      initialRoute: '/splash',
+      getPages: [
+        GetPage(name: '/splash', page: () => SplashScreen()),
+        GetPage(name: '/login', page: () => const LoginScreen()),
+        GetPage(name: '/beranda', page: () => const BerandaScreen()),
+      ],
       theme: ThemeData(
-        brightness: Brightness.dark, 
-        primaryColor: const Color.fromARGB(255, 40, 40, 40), 
-        scaffoldBackgroundColor: const Color.fromARGB(255, 40, 40, 40), 
+        brightness: Brightness.dark,
+        primaryColor: const Color.fromARGB(255, 40, 40, 40),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 40, 40, 40),
         cardColor: Colors.blueGrey[800],
-        textTheme: TextTheme(
-          bodyLarge: const TextStyle(color: Colors.white), // Warna teks utama
-          bodyMedium: TextStyle(color: Colors.grey[300]), // Warna teks sekunder
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.white),
+          bodyMedium: TextStyle(color: Colors.grey),
         ),
         appBarTheme: const AppBarTheme(
-          backgroundColor: Color.fromARGB(255, 40, 40, 40), // Warna app bar
+          backgroundColor: Color.fromARGB(255, 40, 40, 40),
           titleTextStyle: TextStyle(
             color: Colors.white,
             fontSize: 24.0,
